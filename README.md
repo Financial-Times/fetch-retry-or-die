2dfb736118d2bdd11b6a4883402dd192f6c11a7d
Catalin Anghel <catalin.anghel@iquestgroup.com>
2016-11-15 18:25:31 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # fetch-retry-or-die
-A wrapper around isomorphic-fetch that allows retries in case of request failure 
+A wrapper around [isomorphic-fetch] that allows retries in case of request failure 
+
+### Install
+```sh
+npm install [-S] fetch-retry-or-die
+```
+
+### Usage
+```javascript
+const fetch = require('fetch-retry-or-die');
+```
+```javascript
+let url = 'http://some.url'
+fetch(url, {maxRetries: 5, retryDelay: 500})
+  .then((response) => {
+    // do something with the response
+    ...
+  });
+```
+
+### Options
+* `maxRetries`: the maximum number of allowed retries. Defaults to 1.
+* `retryDelay`: the delay between retries in milliseconds. Defaults to 100.
+
+[//]: # 
+   [isomorphic-fetch]: <https://github.com/matthew-andrews/isomorphic-fetch>
---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+const logger = require('@financial-times/n-logger');
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+module.exports = (url, opt) => {
+
+    const maxRetries = opt ? opt.maxRetries || 1 : 1;
+    const retryDelay = opt ? opt.retryDelay || 100 : 100;
+
+    const fetchRetryOrDie = (resolve, reject) => {
+        let retryCount = 0;
+        const doRequest = () => {
+            fetch(url, opt)
+                .then((response) => {
+                    let statusStr = response.status.toString();
+                    if (statusStr.indexOf('2') === 0 || statusStr === '404') {
+                        resolve(response);
+                    } else {
+                        throw new Error(`${response.statusText}: ${response.status}`);
+                    }
+                })
+                .catch((err) => {
+                    if (retryCount < maxRetries) {
+                        retryCount++;
+                        logger.warn({operation: 'fetch-retry-or-die', msg: `Retry #${retryCount}`, url});
+                        setTimeout(doRequest, retryDelay);
+                    } else {
+                        logger.error({operation: 'fetch-retry-or-die', err, url});
+                        reject(err);
+                    }
+                });
+        };
+        doRequest();
+    };
+
+    return new Promise(fetchRetryOrDie);
+};
